2dc13d4e9080dc25413eafcdcb56bc5fdf3ba3e7
NickMoignard <nicklmoignard@gmail.com>
2016-05-25 16:49:57 +1000

    added a simple express router to the web app

--- server.js
+++ server.js
@@ -3,10 +3,29 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+// get an instance of the express router
+var adminRouter = express.Router();
+
 // send our index.html file to the user for the home page
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+// admin main page. the dashboard (http://localhost:1337/admin)
+adminRouter.get('/', function(req, res) {
+	res.send('i am the dashboard')
+});
+// users page. (http://localhost:1337/admin/users)
+adminRouter.get('/users', function(req, res) {
+	res.send('all the users!')
+});
+// posts page. (http://localhost:1337/admin/posts)
+adminRouter.get('/posts', function(req, res) {
+	res.send('all of the posts')
+});
+
+// apply the routes to the application
+app.use('/admin', adminRouter)
 
 //start the server
 app.listen(1337);
